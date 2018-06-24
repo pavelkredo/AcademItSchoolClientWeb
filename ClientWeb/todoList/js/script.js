91f372c6c22819aba7e09e73f9857e75cffa3f3a
pavelkredo <pavelkredo@mail.ru>
2018-06-24 13:30:44 +0700

    Учел замечания в задаче "Работа с DOM"

--- ClientWeb/todoList/js/script.js
+++ ClientWeb/todoList/js/script.js
@@ -4,6 +4,7 @@
         var textbox = document.body.querySelector(".addtext");
 
         onclick.addEventListener("click", enterResult, false);
+        var ul = document.createElement("ul");
 
         function enterResult() {
             var text = textbox.value;
@@ -11,28 +12,23 @@
             if (text !== "") {
                 textbox.value = null;
 
-                var note = document.createElement("li");
-                note.innerText = text;
-
-                var ul = document.createElement("ul");
-                ul.appendChild(note);
+                var li = document.createElement("li");
+                li.innerText = text;
 
                 var deleteButton = document.createElement("input");
                 deleteButton.type = "button";
                 deleteButton.className = "deleteButton";
                 deleteButton.value = "Удалить";
 
-                ul.appendChild(deleteButton);
+                li.appendChild(deleteButton);
 
-                document.body.appendChild(ul);
+                ul.appendChild(li);
 
-                deleteButton.addEventListener("click", deleteButtonsFunc(ul), false);
+                deleteButton.addEventListener("click", function() {
+                    li.remove();
+                }, false);
             }
         }
 
-        function deleteButtonsFunc(ul) {
-            return function() {
-                ul.remove();
-            }
-        }
+        document.body.appendChild(ul);
     });
